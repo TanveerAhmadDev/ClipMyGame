d4e1cc63e0985a657289b42e680aecf6959a6c28
TanveerAhmadDev <tanveeera84@gmail.com>
2026-08-11 20:57:48 +0500

    fix: add additional CORS origin for deployment

--- backend/api/index.js
+++ backend/api/index.js
@@ -15,7 +15,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://192.168.0.106:5173"],
+    origin: [
+      "http://localhost:5173",
+      "http://192.168.0.106:5173",
+      "https://clip-my-game-rho.vercel.app",
+    ],
     credentials: true,
   }),
 );
